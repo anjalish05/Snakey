c937a3437a375e3f95080cf12105ca617d523d9c
anjalish05 <singhanjaliajab115@gmail.com>
2022-06-09 00:44:05 +0530

    game done

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <conio.h>
+#include <windows.h> // to activate Sleep() function
 
 using namespace std;
 
@@ -11,13 +12,16 @@ const int height = 20;
 int x, y; 
 // variables for fruit position, and a score variable 
 int fruitX, fruitY, score;
+// tail of the snake, there are several segments - Ooooo
+// As the head moves, the body should move along 
+int tailX[100], tailY[100];
+int nTail; // length of the tail
 
 // Suppose 'a' is the key, which on pressing moves the snake to left, but it makes the snake move continuously towards left, rather than making us click 'a' multiple times
 // So, we need to track the directions, when we press a key, it needs to change the direction
 // this is enumeration
 enum eDirecton {STOP = 0, LEFT, RIGHT, UP, DOWN};
 eDirecton dir; // variable for direction
-
 
 void Setup() {
     gameOver = false;
@@ -48,16 +52,25 @@ void Draw() {
                 cout << "F";
             }
             else {
-                cout << " ";
+                bool print = false;
+                for (int k = 0; k < nTail; k++) {
+                    if(tailX[k] == j && tailY[k] == i) {
+                        cout << "o"; 
+                        print = true;
+                    }
+                }
+                if (!print) {
+                        cout << " ";
+                    }
             }
-            if(j == width-1) {
+            if (j == width-1) {
                 cout << "#";
             }
         }
         cout << endl;
     }
 
-    for(int i = 0; i < width; i++){
+    for(int i = 0; i < width+2; i++){
         cout << "#"; // map wall
     }
     cout << endl;
@@ -93,6 +106,21 @@ void Input() {
 }
 
 void Logic() {
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x; // to follow the head
+    tailY[0] = y; 
+
+    for (int i = 1; i < nTail; i++) {
+        prev2X = tailX[i]; // current position of segment 
+        prev2Y = tailY[i];
+        tailX[i] = prevX; // change to previous 
+        tailY[i] = prevY;
+        prevX = prev2X; // update prevX
+        prevY = prev2Y;
+    }
+
     switch(dir) {
         // As we go left, 'x' coordinates are decreased and so on and so forth 
         // with other keys
@@ -115,10 +143,30 @@ void Logic() {
 
     // below is the snippet for when head touches the walls, the game is going to end
 
-    if (x > width || x < 0 || y > height || y < 0) {
-        gameOver = true;
+    // if (x > width || x < 0 || y > height || y < 0) {
+    //     gameOver = true;
+    // }
+    // to start from the lest wall if head hits the right wall
+    if (x >= width) {
+        x = 0;
+    }
+    else if (x < 0) {
+        x = width - 1;
+    }
+    if (y >= height) {
+        y = 0;
+    }
+    else if (y < 0) {
+        y = height - 1;
+    }
+
+    for (int i = 0; i < nTail; i++) {
+        if (tailX[i] == x && tailY[i] == y) {
+            gameOver = true;
+        }
     }
     if (x == fruitX && y == fruitY) {
+        nTail++; // Increase the tail size
         score += 10;
         // to randomly allocate the fruit again after being hit by it
         fruitX = rand() % width;
@@ -134,7 +182,7 @@ int main() {
         Draw();
         Input();
         Logic();
-        // Sleep(); sleep(10): it slows our game!
+        Sleep(50); // sleep(10): it slows our game!
     }
 
 
